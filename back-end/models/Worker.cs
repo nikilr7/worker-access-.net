6c3e6b8c03fddc18737519377745577c08c0066b
nikilr7 <nikilraja11@gmail.com>
2026-03-13 01:18:50 +0530

    started the backend works

---
+++ back-end/models/Worker.cs
@@ -0,0 +1,16 @@
+﻿namespace Back_end.models
+{
+    public class Worker : Entity
+    {
+        public virtual String Name { get; set; }
+        public virtual DateTime DateOfBirth { get; set; }
+        public virtual int age { get; set; }
+        public virtual String PhoneNumber { get; set; }
+        public virtual String Email { get; set; }
+        public virtual float? Salary { get; set; }
+        public virtual int Experience { get; set; }
+
+
+
+    }
+}
